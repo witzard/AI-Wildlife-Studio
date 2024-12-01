3c7cc73117b28cf7180ee887b9965101a324836f
winzent <mynameiswittawin2004@gmail.com>
2024-12-01 20:03:18 +0700

    npm install update

--- ai_wildlife_studio/app/page.tsx
+++ ai_wildlife_studio/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <section className="relative bg-[url(https://th.bing.com/th/id/OIG1.sO98ryHrV13MfdaPIWZu?pid=ImgGn)] bg-cover bg-center bg-no-repeat">
       <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
       <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
-          <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+        <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
             Let us find your
 
